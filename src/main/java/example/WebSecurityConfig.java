484c6a1392a2684cb1e3122188ce87efa9af18a1
haljik <se.k.hal7700@gmail.com>
2015-06-02 23:53:17 +0900

    basic 認証でセキュリティの動作を確認

--- src/main/java/example/WebSecurityConfig.java
+++ src/main/java/example/WebSecurityConfig.java
@@ -14,6 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
         http.authorizeRequests()
                 .antMatchers("/secure/**/*").authenticated()
                 .antMatchers("/").permitAll();
