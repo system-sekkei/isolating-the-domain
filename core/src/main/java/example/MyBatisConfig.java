95e7313710924ded25855802df5b467eced244e5
haljik <se.k.hal7700@gmail.com>
2015-06-04 16:53:28 +0900

    MyBatis、テストの設定を追加

---
+++ core/src/main/java/example/MyBatisConfig.java
@@ -0,0 +1,27 @@
+package example;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by haljik on 15/06/04.
+ */
+@Configuration
+@MapperScan(basePackages = "example.datasource")
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setTypeHandlersPackage("example.datasource.infrastructure.mybatis");
+        sessionFactory.setConfigLocation(new ClassPathResource("mybatis.xml"));
+        return sessionFactory.getObject();
+    }
+}
---
+++ core/src/test/groovy/example/TestConfiguration.groovy
@@ -0,0 +1,12 @@
+package example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TestConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestConfiguration.class, args);
+    }
+}
