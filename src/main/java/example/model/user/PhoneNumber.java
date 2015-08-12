d0384ed877e19fe7a3741f68d7eada799960ed19
setoguchi takuma <setoguchi.takuma@gmail.com>
2015-08-12 22:44:56 +0900

    不要なVaidationGroup用のinterfaceを削除

--- src/main/java/example/model/user/PhoneNumber.java
+++ src/main/java/example/model/user/PhoneNumber.java
@@ -1,6 +1,5 @@
 package example.model.user;
 
-import example.model.user.validation.OnRegisterWithPhoneNumber;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 import org.hibernate.validator.constraints.NotBlank;
@@ -8,7 +7,7 @@ import org.hibernate.validator.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 public class PhoneNumber {
-    @NotBlank(message = "電話番号を入力してください", groups = OnRegisterWithPhoneNumber.class)
+    @NotBlank(message = "電話番号を入力してください")
     @Pattern(regexp = "([0-9]{2,4}-[0-9]{2,4}-[0-9]{2,4})?", message = "xx-xxxx-xxxxの形式で入力してください")
     String value;
 
--- src/main/java/example/model/user/validation/OnRegisterWithPhoneNumber.java
+++
@@ -1,6 +0,0 @@
-package example.model.user.validation;
-
-import javax.validation.groups.Default;
-
-public interface OnRegisterWithPhoneNumber extends Default {
-}
