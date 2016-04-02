b20636f4859a3f4e9c360d6ed4daddda4dd763be
masuda220 <masuda220@gmail.com>
2016-04-02 22:03:55 +0900

    日付文字列を日付に変換する DateText クラスを追加

---
+++ src/main/java/example/fundamentals/DateText.java
@@ -0,0 +1,34 @@
+package example.fundamentals;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.IllegalFormatException;
+
+/**
+ * Created by Masuda on 2016/04/02.
+ */
+public class DateText {
+    String source;
+
+    public DateText(String source) {
+        this.source = source;
+    }
+
+    static final DateTimeFormatter[] formatters = {
+        DateTimeFormatter.ofPattern("yyyy/MM/dd"),
+        DateTimeFormatter.ofPattern("yyyy-MM-dd"),
+        DateTimeFormatter.ofPattern("yyyyMMdd")
+    };
+
+    public LocalDate toLocalDate() {
+        for (DateTimeFormatter each : formatters) {
+            try {
+                return LocalDate.parse(source, each);
+            } catch (DateTimeParseException e) {
+                continue;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
---
+++ src/main/java/example/model/user/DateOfBirth.java
@@ -0,0 +1,17 @@
+package example.model.user;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Masuda on 2016/04/02.
+ */
+public class DateOfBirth {
+
+    LocalDate date ;
+
+    String source;
+
+    public DateOfBirth(String source) {
+
+    }
+}
