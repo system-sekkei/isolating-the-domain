f84fb6bca0774fe2f61fd63772c7a5a56e692738
masuda <masuda220@gmail.com>
2016-04-05 15:21:55 +0900

    DateText の初期化と事前条件の改善

--- src/main/java/example/fundamentals/DateText.java
+++ src/main/java/example/fundamentals/DateText.java
@@ -1,9 +1,10 @@
 package example.fundamentals;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.IllegalFormatException;
+import java.util.HashSet;
 
 /**
  * Created by Masuda on 2016/04/02.
@@ -12,23 +13,35 @@ public class DateText {
     String source;
 
     public DateText(String source) {
+        assert source != null ;
         this.source = source;
     }
 
-    private static final DateTimeFormatter[] formatters = {
-        DateTimeFormatter.ofPattern("yyyy/MM/dd"),
-        DateTimeFormatter.ofPattern("yyyy-MM-dd"),
-        DateTimeFormatter.ofPattern("yyyyMMdd")
-    };
+    private static final String[] patterns;
+
+    static {
+        patterns = new String[]{
+                "yyyy/MM/dd",
+                "yyyy-MM-dd",
+                "yyyyMMdd"
+        };
+    }
+
+    private static final HashSet<DateTimeFormatter> formatters;
+
+    static {
+        formatters = new HashSet<DateTimeFormatter>();
+        for( String each : patterns ) formatters.add(DateTimeFormatter.ofPattern(each));
+    }
 
     public LocalDate toLocalDate() {
         for (DateTimeFormatter each : formatters) {
             try {
                 return LocalDate.parse(source, each);
             } catch (DateTimeParseException e) {
-                // ���s������A���̃t�H�[�}�b�g�Ŏ���
+                // 失敗したら次のパターンを試す
             }
         }
-        throw new IllegalArgumentException();
+        throw new DateTimeException("日付の形式が正しくありません");
     }
 }
