767312a9fdaba91d14b704428a03640842b80543
masuda220 <masuda220@gmail.com>
2016-04-09 09:30:51 +0900

    登録コントローラの簡素化

--- src/main/java/example/web/user/RegisterController.java
+++ src/main/java/example/web/user/RegisterController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("user/registration")
+@RequestMapping("user/register")
 @SessionAttributes("user")
-class RegistrationController {
+class RegisterController {
 
     private static final String[] allowFields ;
     static {
@@ -51,48 +51,38 @@ class RegistrationController {
 
     //入り口 session attribute をクリアする
     @RequestMapping(method = RequestMethod.GET)
-    String clearSessionAttribute(SessionStatus sessionStatus,@RequestParam(value="userId") String userId) {
+    String clearSessionAttribute(SessionStatus sessionStatus) {
         sessionStatus.setComplete();
         return "forward:/user/register/input";
     }
 
     @RequestMapping(value="/input", method = RequestMethod.GET)
-    String input(@PathVariable(value="userId") String userId,Model model) {
+    String input(Model model) {
         User user = userService.prototype();
         model.addAttribute("user", user); //sessionAttributeに格納
-        return "user/update/input";
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    String start() {
-        return "user/registration/register";
+        return "user/register/input";
     }
 
     @RequestMapping(value = "/confirm", method = RequestMethod.POST)
     String confirm(@Validated(OnRegister.class) @ModelAttribute User user, BindingResult result) {
-        if (result.hasErrors()) return "user/registration/register";
+        if (result.hasErrors()) return "user/register/input";
         if (userService.findById(user.getId()) != null) {
             result.reject("", new Object[]{user.getId().getValue()}, "ユーザー {0} は登録済みです");
-            return "user/registration/register";
+            return "user/register/register";
         }
-        return "user/registration/confirm";
+        return "user/register/confirm";
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
-    String register(@Validated(OnRegister.class) @ModelAttribute User user, BindingResult result, RedirectAttributes attributes) {
-        if (result.hasErrors()) return "user/registration/register";
-        if (userService.findById(user.getId()) != null) {
-            result.reject("", new Object[]{user.getId().getValue()}, "ユーザー {0} は登録済みです");
-            return "user/registration/register";
-        }
+    String register(@ModelAttribute User user, RedirectAttributes attributes) {
         userService.register(user);
         attributes.addFlashAttribute("userId", user.getId().getValue());
-        return "redirect:/user/registration/complete";
+        return "redirect:/user/register/complete";
     }
 
     @RequestMapping(value = "/complete", method = RequestMethod.GET)
     String complete(SessionStatus status) {
         status.setComplete();
-        return "user/registration/complete";
+        return "user/register/complete";
     }
 }
