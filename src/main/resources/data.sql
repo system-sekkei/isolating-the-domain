b2da4fda0d67394e49accb955448650ff0bf6e65
masuda220 <masuda220@gmail.com>
2016-04-14 14:54:57 +0900

    users テーブルの全カラムを NOT NULL に。テストデータの件数を減らし、項目を増やす。

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,9 @@
-INSERT INTO users.users (user_id, name) VALUES
-  ('fukawa_teruyoshi@example.com', '布川 光良'),
-  ('kuriyama_yuino@example.com', '栗山 友以乃'),
-  ('inui_shigeki@example.com', '乾 茂樹'),
-  ('fujimura_kaoru@example.com', '藤村 薫'),
-  ('ijuuin_ken@example.com', '伊集院 建'),
-  ('yamato_michiko@example.com', '大和 路子'),
-  ('miyake_yukiya@example.com', '三宅 有起哉'),
-  ('kameda_kousuke@example.com', '亀田 浩介'),
-  ('nagashima_kei@example.com', '永島 圭'),
-  ('saitou_yuusuke@example.com', '斉藤 勇介'),
-  ('kawase_ryouko@example.com', '川瀬 涼子'),
-  ('umemoto_yuuichi@example.com', '梅本 祐一'),
-  ('nemoto_hiroaki@example.com', '根本 博明'),
-  ('kamiya_takeo@example.com', '神谷 丈雄'),
-  ('tabata_jin@example.com', '田端 仁'),
-  ('nakagawa_sora@example.com', '中川 夏空');
-
+INSERT INTO users.users
+(user_id, name, phone_number, date_of_birth, gender)
+VALUES
+ ('fukawa_teruyoshi@example.com', '布川 光良', '03-1234-5678','1988-04-08','男性'),
+ ('kuriyama_yuino@example.com', '栗山 友以乃', '03-1234-5678','1988-04-08','女性'),
+ ('fujimura_kaoru@example.com', '藤村 薫', '03-1234-5678','1988-04-08','男性'),
+ ('ijuuin_ken@example.com', '伊集院 建', '03-1234-5678','1988-04-08','男性'),
+ ('yamato_michiko@example.com', '大和 路子', '03-1234-5678','1988-04-08','女性'),
+ ('miyake_yukiya@example.com', '三宅 有起子', '03-1234-5678','1988-04-08','女性');
