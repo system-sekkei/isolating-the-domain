e3b2ec4c82a4bf7bc29cf74b5dd44f30ed802925
haljik <se.k.hal7700@gmail.com>
2016-04-18 14:22:20 +0900

    SpringBootのVersionを1.3.3へ

--- src/main/java/example/WebSecurityConfig.java
+++ src/main/java/example/WebSecurityConfig.java
@@ -2,14 +2,14 @@ package example;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 /**
  * Created by haljik on 15/02/03.
  */
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
