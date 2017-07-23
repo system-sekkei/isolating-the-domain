40768e0cc592db7c3336943cc41d83cf30e141c7
masuda220 <masuda220@gmail.com>
2017-07-23 14:39:53 +0900

    bootstrap からsemantic ui に変更

--- src/main/java/example/domain/model/user/DateOfBirth.java
+++ src/main/java/example/domain/model/user/DateOfBirth.java
@@ -1,15 +1,7 @@
 package example.domain.model.user;
 
-import example.domain.fundamentals.DateText;
-
-import javax.validation.constraints.AssertTrue;
-import javax.validation.constraints.NotNull;
-import java.time.DateTimeException;
 import java.time.LocalDate;
 
-/**
- * Created by Masuda on 2016/04/02.
- */
 public class DateOfBirth {
 
     //@NotNull(message = "誕生日を入力してください。")
