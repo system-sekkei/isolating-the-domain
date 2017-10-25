67d36e01095b167599483de17314c1d9365dd995
yamaki <yamaki@wh-plus.com>
2017-10-25 20:09:02 +0900

    電話番号の桁数が14桁になるとシステムが落ちるので、桁数チェックを追加

--- src/main/java/example/domain/model/user/PhoneNumber.java
+++ src/main/java/example/domain/model/user/PhoneNumber.java
@@ -3,11 +3,13 @@ package example.domain.model.user;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class PhoneNumber {
 
     @NotBlank(message = "電話番号を入力してください")
     @Pattern(regexp = "([0-9]{2,4}-[0-9]{2,4}-[0-9]{2,4})?", message = "xx-xxxx-xxxxの形式で入力してください")
+    @Size(min = 8, max = 13, message = "桁数は8桁以上13桁以下で入力してください")
     String value = "";
 
     @Override
