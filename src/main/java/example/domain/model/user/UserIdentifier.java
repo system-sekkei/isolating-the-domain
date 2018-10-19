47868b63b6da6cc386750d9c993927d466dc061c
山根英次 <eijiyamane@gmail.com>
2018-10-19 17:39:57 +0900

    メアドのバリデーションを外す

--- src/main/java/example/domain/model/user/UserIdentifier.java
+++ src/main/java/example/domain/model/user/UserIdentifier.java
@@ -1,12 +1,10 @@
 package example.domain.model.user;
 
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
 public class UserIdentifier {
 
-    @NotBlank(message = "メールアドレスを入力してください")
-    @Email(message = "メールアドレスが正しくありません。")
+    @NotBlank(message = "IDを入力してください")
     String value = "";
 
     public UserIdentifier() {
