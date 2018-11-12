13b1138739f2a9393ddeb1b603b591b2cf8b0733
irof <irof@hogedriven.net>
2018-11-12 16:29:15 +0900

    労働基準法パッケージを追加

---
+++ src/main/java/example/domain/model/labour_standards_law/ExtraPayRate.java
@@ -0,0 +1,14 @@
+package example.domain.model.labour_standards_law;
+
+import java.math.BigDecimal;
+
+/**
+ * 割増率
+ */
+public class ExtraPayRate {
+    BigDecimal value;
+
+    public ExtraPayRate(String value) {
+        this.value = new BigDecimal(value);
+    }
+}
---
+++ src/main/java/example/domain/model/labour_standards_law/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 労働基準法
+ */
+package example.domain.model.labour_standards_law;
