06b1580dbe4025ffac6ec8941a53bb3fa7dcb39e
irof <irof@hogedriven.net>
2018-11-24 15:10:32 +0900

    時給登録画面とControllerをつなげた

--- src/main/java/example/presentation/controller/worker/WageController.java
+++ src/main/java/example/presentation/controller/worker/WageController.java
@@ -1,18 +1,44 @@
 package example.presentation.controller.worker;
 
 
+import example.application.service.worker.WorkerQueryService;
+import example.domain.model.contract.HourlyWage;
+import example.domain.model.worker.Worker;
+import example.domain.model.worker.WorkerNumber;
+import example.domain.type.date.Date;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("worker/wage")
+@RequestMapping("worker/wage/{workerNumber}")
 class WageController {
 
-    @GetMapping("")
-    public String hello(Model model) {
+    WorkerQueryService workerQueryService;
+
+    public WageController(WorkerQueryService workerQueryService) {
+        this.workerQueryService = workerQueryService;
+    }
+
+    @ModelAttribute("worker")
+    Worker worker(@PathVariable(value = "workerNumber") WorkerNumber workerNumber) {
+        return workerQueryService.choose(workerNumber);
+    }
+
+    @GetMapping
+    public String init(Worker worker,
+                       Model model) {
         return "worker/wage/form";
+    }
+
+    @PostMapping
+    public String register(Worker worker,
+                           @RequestParam("startDate") Date startDate,
+                           @RequestParam("hourlyWage") HourlyWage hourlyWage,
+                           Model model) {
+        // TODO 時給の登録
+
+        return "redirect:/attendance/workers";
     }
 }
