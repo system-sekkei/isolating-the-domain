4dbe7a3833e41fbf8db4e104f45b16589ee7b7ec
yukieen <lpczclt@gmail.com>
2018-12-05 10:29:26 +0900

    画面要素の変更にともなうテストの変更

--- cypress/integration/smoke/smoke.js
+++ cypress/integration/smoke/smoke.js
@@ -6,11 +6,11 @@ context('isolating-the-domain', () => {
     cy.title().should('eq', 'ダッシュボード')
 
     cy.get('a').contains('従業員').click()
-    cy.title().should('contains', '従業員一覧')
+    cy.title().should('contains', '従業員の一覧')
 
     // 従業員登録
     cy.contains('従業員の新規登録').click()
-    cy.title().should('contains', '従業員の新規登録')
+    cy.title().should('contains', '従業員情報の新規登録')
     //何も入力せずにバリデーションが走っていることを確認
     cy.get('.button').contains('確認する').click()
     cy.get('div.error').should((errorDivs) =>{
@@ -24,7 +24,7 @@ context('isolating-the-domain', () => {
     cy.get('.button').contains('登録する').click()
     cy.title().should('contains', '完了')
     cy.get('a').contains('従業員一覧へ').click()
-    cy.title().should('eq', '従業員一覧')
+    cy.title().should('eq', '従業員の一覧')
 
     // 従業員更新
     cy.get('tbody > tr > td').contains('テスト太郎').parent().within(() => {
@@ -35,16 +35,14 @@ context('isolating-the-domain', () => {
     cy.get('.button').contains('編集の確認').click()
     cy.title().should('contains', '確認')
     cy.get('.button').contains('保存する').click()
-    cy.get('a').contains('従業員一覧へ').click()
-    cy.title().should('eq', '従業員一覧')
 
     cy.get('a').contains('ダッシュボード').click()
     cy.title().should('eq', 'ダッシュボード')
 
     // 勤務時間入力
-    cy.get('a').contains('勤務時間入力').click()
+    cy.get('a').contains('勤務時間の入力').click()
     cy.title().should('contains', '入力')
-    cy.get('.button').contains('登録する').click()
+    cy.get('.button').contains('登録').click()
     cy.title().should('contains', '勤務時間の一覧')
 
     cy.get('a').contains('ダッシュボード').click()
@@ -52,20 +50,20 @@ context('isolating-the-domain', () => {
 
     // 給与計算
     cy.get('a').contains('給与計算').click()
-    cy.title().should('contains', '給与計算一覧')
+    cy.title().should('contains', '給与計算の一覧')
     cy.get('tbody > tr > td').contains('テスト次郎').parent().within(() => {
         cy.get('.button').contains('勤務時間').click()
     })
     cy.title().should('contains', '一覧')
     cy.get('a').contains('給与計算一覧へ').click()
-    cy.title().should('contains', '給与計算一覧')
+    cy.title().should('contains', '給与計算の一覧')
 
     // 契約終了
     cy.get('a').contains('ダッシュボード').click()
     cy.title().should('eq', 'ダッシュボード')
 
     cy.get('a').contains('従業員').click()
-    cy.title().should('contains', '従業員一覧')
+    cy.title().should('contains', '従業員の一覧')
 
     cy.get('tbody > tr > td').contains('テスト次郎').parent().within(() => {
         cy.get('.button').contains('変更').click()
@@ -74,8 +72,6 @@ context('isolating-the-domain', () => {
     cy.get('.button').contains('削除の確認').click()
     cy.title().should('contains', '確認')
     cy.get('.button').contains('削除').click()
-    cy.contains('従業員一覧へ').click()
-    cy.title().should('contains', '従業員一覧')
     })
 })
 
