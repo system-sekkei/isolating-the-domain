b826a27037586a22a2f3c7850c21ec0e4f5d1a3f
haljik <seiji.kawakami@sora-works.com>
2018-12-06 17:57:07 +0900

    YearMonthの内部型をj.t.YearMonthへ

--- src/main/java/example/domain/type/date/YearMonth.java
+++ src/main/java/example/domain/type/date/YearMonth.java
@@ -1,9 +1,8 @@
 package example.domain.type.date;
 
-import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,45 +10,39 @@ import java.util.stream.IntStream;
  * 年月
  */
 public class YearMonth {
-    Year year;
-    Month month;
+    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM");
+    java.time.YearMonth value;
 
     public YearMonth(Year year, Month month) {
-        this.year = year;
-        this.month = month;
+        this.value = java.time.YearMonth.of(year.value(), month.value());
     }
 
     public YearMonth(Integer year, Integer month) {
         this(new Year(year), Month.of(month));
     }
 
-    static Pattern yearMonthPattern = Pattern.compile("(\\d{4})-(\\d{1,2})");
-
     public YearMonth(String yearMonth) {
-        Matcher m = yearMonthPattern.matcher(yearMonth);
-        if (m.matches()) {
-            this.year = new Year(m.group(1));
-            this.month = Month.of(m.group(2));
-        } else {
-            throw new IllegalArgumentException("月のフォーマットが誤っています");
+        try {
+            this.value = java.time.YearMonth.parse(yearMonth, FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("月のフォーマットが誤っています", e);
         }
     }
 
     public Year year() {
-        return year;
+        return new Year(value.getYear());
     }
 
     public Month month() {
-        return month;
+        return Month.of(value.getMonthValue());
     }
 
     public Date start() {
-        return new Date(LocalDate.of(year().value(), month().value(), 1));
+        return new Date(value.atDay(1));
     }
 
     public Date end() {
-        LocalDate endOfMonth = java.time.YearMonth.of(year.value(), month().value()).atEndOfMonth();
-        return new Date(endOfMonth);
+        return new Date(value.atEndOfMonth());
     }
 
     public List<Date> days() {
@@ -58,6 +51,6 @@ public class YearMonth {
     }
 
     public String toString() {
-        return String.format("%s-%s", year, month);
+        return value.format(FORMATTER);
     }
 }
