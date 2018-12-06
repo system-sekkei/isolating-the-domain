b33c214a468db6a0240ed826611b33140a202696
haljik <seiji.kawakami@sora-works.com>
2018-12-06 17:35:25 +0900

    月末日をj.t.YearMonthを使って算出するように

--- src/main/java/example/domain/type/date/YearMonth.java
+++ src/main/java/example/domain/type/date/YearMonth.java
@@ -48,8 +48,8 @@ public class YearMonth {
     }
 
     public Date end() {
-        LocalDate tmp = LocalDate.of(year().value(), month().value() % Month.values().length + 1, 1);
-        return new Date(tmp.minusDays(1L));
+        LocalDate endOfMonth = java.time.YearMonth.of(year.value(), month().value()).atEndOfMonth();
+        return new Date(endOfMonth);
     }
 
     public List<Date> days() {
