b906d84fca9e418efddd7720a1cf690cef3946be
yukieen <lpczclt@gmail.com>
2018-12-10 15:48:14 +0900

    従業員の変更ボタンを詳細画面に移動

--- cypress/integration/smoke/smoke.js
+++ cypress/integration/smoke/smoke.js
@@ -32,9 +32,8 @@ context('isolating-the-domain', () => {
     cy.get('.button').contains('一覧に戻る').click()
 
     // 従業員更新
-    cy.get('tbody > tr > td > a > span').contains('テスト太郎').parent().parent().parent().within(() => {
-        cy.get('.button').contains('変更').click()
-    })
+    cy.get('tbody > tr > td > a').contains('テスト太郎').click()
+    cy.get('.button').contains('変更').click()
     cy.title().should('contains', '変更')
     cy.get('#name\\.value').clear().type('テスト次郎')
     cy.get('.button').contains('変更の確認').click()
@@ -70,10 +69,11 @@ context('isolating-the-domain', () => {
     cy.get('a').contains('従業員').click()
     cy.title().should('contains', '従業員の一覧')
 
-    cy.get('tbody > tr > td > a > span').contains('テスト次郎').parent().parent().parent().within(() => {
-        cy.get('.button').contains('変更').click()
-    })
-    cy.title().should('contains', '変更')
+    //従業員削除
+    cy.get('tbody > tr > td > a').contains('テスト次郎').click()
+    cy.title().should('contains', '従業員情報の詳細')
+    cy.get('.button').contains('変更').click()
+    cy.title().should('contains', '従業員情報の変更')
     cy.get('.button').contains('削除の確認').click()
     cy.title().should('contains', '確認')
     cy.get('.button').contains('削除').click()
