24109ca0104fbdeda150e95ea16a87c589f7f794
yukieen <lpczclt@gmail.com>
2018-12-11 12:04:02 +0900

    従業員の削除確認をモーダルに変更した（コントローラー）

--- src/main/java/example/presentation/controller/worker/DeleteController.java
+++ src/main/java/example/presentation/controller/worker/DeleteController.java
@@ -5,12 +5,9 @@ import example.application.service.worker.WorkerRecordService;
 import example.domain.model.worker.Worker;
 import example.domain.model.worker.WorkerNumber;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("workers/{workerNumber}/delete")
@@ -19,30 +16,11 @@ public class DeleteController {
     WorkerRecordService workerRecordService;
     WorkerQueryService workerQueryService;
 
-    @GetMapping(value = "view")
-    String show(@PathVariable(value = "workerNumber") WorkerNumber workerNumber, Model model) {
-        Worker worker = workerQueryService.choose(workerNumber);
-        model.addAttribute("worker", worker);
-
-        return "worker/delete/confirm";
-    }
-
     @GetMapping(value = "")
-    String deleteThenRedirect(@PathVariable(value = "workerNumber") WorkerNumber workerNumber,
-                              Model model, RedirectAttributes attributes) {
+    String deleteThenRedirect(@PathVariable(value = "workerNumber") WorkerNumber workerNumber) {
         Worker worker = workerQueryService.choose(workerNumber);
         workerRecordService.expireContract(worker);
-
-        attributes.addAttribute("name", worker.name().toString());
-
-        return "redirect:/workers/someone/delete/completed";
-    }
-
-    @GetMapping(value = "/completed")
-    String showResult(Model model,
-                      @RequestParam("name") String name) {
-        model.addAttribute("name", name);
-        return "worker/delete/result";
+        return "redirect:/workers";
     }
 
     DeleteController(WorkerRecordService workerRecordService, WorkerQueryService workerQueryService) {
