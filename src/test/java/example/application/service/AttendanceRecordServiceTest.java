5b065a347bc0564cbce65510d4f97ad0a67e03fc
yukieen <lpczclt@gmail.com>
2018-12-13 12:34:00 +0900

    勤怠情報のテストをまとめた

--- src/test/java/example/application/service/AttendanceRecordServiceTest.java
+++ src/test/java/example/application/service/AttendanceRecordServiceTest.java
@@ -5,7 +5,10 @@ import example.application.service.attendance.AttendanceQueryService;
 import example.application.service.attendance.AttendanceRecordService;
 import example.application.service.worker.WorkerQueryService;
 import example.domain.model.attendance.*;
+import example.domain.model.worker.ContractingWorkers;
+import example.domain.model.worker.Worker;
 import example.domain.model.worker.WorkerNumber;
+import example.domain.type.date.Date;
 import example.domain.type.time.ClockTime;
 import example.domain.type.time.Minute;
 import org.junit.jupiter.api.Test;
@@ -13,39 +16,61 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * 勤怠情報のテスト
+ */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
 class AttendanceRecordServiceTest {
+
     @Autowired
     WorkerQueryService workerQueryService;
     @Autowired
-    AttendanceRecordService sut;
+    AttendanceRecordService attendanceRecordService;
     @Autowired
     AttendanceQueryService attendanceQueryService;
 
     @Test
-    void register() {
-        WorkerNumber workerNumber = workerQueryService.contractingWorkers().list().get(0).workerNumber();
+    @Transactional
+    void 登録した勤怠情報を取得できる() {
+        ContractingWorkers workers = workerQueryService.contractingWorkers();
+        Worker worker = workers.list().get(0);
+        WorkerNumber workerNumber = worker.workerNumber();
 
         int year = 2099;
         int month = 10;
         int day = 20;
+        WorkDay workDay = new WorkDay(LocalDate.of(year, month, day));
 
-        WorkDay workDay = new WorkDay(LocalDate.of(year,month,day));
-        Attendance work = new Attendance(workDay, new WorkStartTime(new ClockTime("9:00")), new WorkEndTime(new ClockTime("17:00")), new NormalBreakTime(new Minute(60)), new MidnightBreakTime("0"));
+        Attendance expectAttendance = new Attendance(workDay, new WorkStartTime(new ClockTime("9:00")), new WorkEndTime(new ClockTime("17:00")), new NormalBreakTime(new Minute(60)), new MidnightBreakTime("0"));
+        attendanceRecordService.registerAttendance(workerNumber, expectAttendance);
 
-        sut.registerAttendance(workerNumber, work);
+        MonthlyAttendances monthlyAttendances = attendanceQueryService.findMonthlyAttendances(workerNumber, new WorkMonth(year, month));
+        assertAll(
+                () -> assertEquals(monthlyAttendances.month().toStringWithUnit(), month + "月"),
+                () -> assertEquals(monthlyAttendances.attendances().list().size(), 1)
+        );
 
-        MonthlyAttendances monthlyAttendances = attendanceQueryService.findMonthlyAttendances(workerNumber, new WorkMonth(year,month));
-        Attendance registeredAttendance = monthlyAttendances.attendanceOf(workDay);
-        assertAll(() -> assertTrue(work.workDay().hasSameValue(registeredAttendance.workDay())),
-                () -> assertEquals(work.workTimeRange().start().toString(), registeredAttendance.workTimeRange().start().toString()),
-                () -> assertEquals(work.workTimeRange().end().toString(), registeredAttendance.workTimeRange().end().toString()),
-                () -> assertEquals(work.totalBreakTime().toString(), registeredAttendance.totalBreakTime().toString()));
+        Date startDate = new Date(LocalDate.of(year, month, day));
+        Date endDate = new Date(LocalDate.of(year, month, day));
+        Attendances attendances = attendanceQueryService.getAttendances(workerNumber, startDate, endDate);
+        WorkTime workTime = attendances.summarize();
+        assertAll(
+                () -> assertEquals(attendances.list().size(), 1),
+                () -> assertEquals(workTime.normalTime().toString(), "07:00")
+        );
+
+        Attendance actualAttendance = attendances.list().get(0);
+        assertAll(() -> assertTrue(expectAttendance.workDay().hasSameValue(actualAttendance.workDay())),
+                () -> assertEquals(expectAttendance.workTimeRange().start().toString(), actualAttendance.workTimeRange().start().toString()),
+                () -> assertEquals(expectAttendance.workTimeRange().end().toString(), actualAttendance.workTimeRange().end().toString()),
+                () -> assertEquals(expectAttendance.totalBreakTime().toString(), actualAttendance.totalBreakTime().toString()));
     }
+
 }
