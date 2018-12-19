6634b8f566027e179eb93625e277f1e0d238753e
yukieen <lpczclt@gmail.com>
2018-12-19 11:14:50 +0900

    給与一覧は別URLへ移動したっぽいので消した

--- src/main/java/example/presentation/controller/attendance/AttendanceController.java
+++ src/main/java/example/presentation/controller/attendance/AttendanceController.java
@@ -5,7 +5,6 @@ import example.application.service.attendance.AttendanceRecordService;
 import example.application.service.worker.WorkerQueryService;
 import example.domain.model.attendance.MonthlyAttendances;
 import example.domain.model.attendance.WorkMonth;
-import example.domain.model.worker.ContractingWorkers;
 import example.domain.model.worker.Worker;
 import example.domain.model.worker.WorkerNumber;
 import org.springframework.stereotype.Controller;
@@ -13,9 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 勤務時間の一覧
@@ -32,24 +28,6 @@ public class AttendanceController {
         this.workerQueryService = workerQueryService;
         this.attendanceRecordService = attendanceRecordService;
         this.attendanceQueryService = attendanceQueryService;
-    }
-
-    @GetMapping("workers")
-    String workers(Model model) {
-        ContractingWorkers contractingWorkers = workerQueryService.contractingWorkers();
-        model.addAttribute("workers", contractingWorkers);
-
-        // TODO 型にする
-        Map<String, MonthlyAttendances> map = new HashMap<>();
-        // TODO 入力から
-        WorkMonth month = new WorkMonth(2018, 11);
-        for (Worker worker : contractingWorkers.list()) {
-            MonthlyAttendances monthlyAttendances = attendanceQueryService.findMonthlyAttendances(worker.workerNumber(), month);
-            map.put(worker.workerNumber().toString(), monthlyAttendances);
-        }
-        model.addAttribute("map", map);
-
-        return "attendance/workers";
     }
 
     @GetMapping("{workerNumber}/{yearMonth}")
