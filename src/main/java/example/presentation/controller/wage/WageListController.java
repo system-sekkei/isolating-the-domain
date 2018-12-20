48bf0a3be5c9a98a7abc440a400d7ed0b8b992c1
yukieen <lpczclt@gmail.com>
2018-12-20 14:06:48 +0900

    時給一覧　従業員名を表示する

--- src/main/java/example/presentation/controller/wage/WageListController.java
+++ src/main/java/example/presentation/controller/wage/WageListController.java
@@ -1,8 +1,14 @@
 package example.presentation.controller.wage;
 
+import example.application.service.contract.ContractRecordService;
+import example.application.service.worker.WorkerQueryService;
+import example.domain.model.worker.Worker;
+import example.domain.model.worker.WorkerNumber;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,6 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("wages/{workerNumber}")
 public class WageListController {
+
+    WorkerQueryService workerQueryService;
+
+    public WageListController(WorkerQueryService workerQueryService, ContractRecordService contractRecordService) {
+        this.workerQueryService = workerQueryService;
+    }
+
+    @ModelAttribute("worker")
+    Worker worker(@PathVariable(value = "workerNumber") WorkerNumber workerNumber) {
+        return workerQueryService.choose(workerNumber);
+    }
 
     @GetMapping("")
     public String list(Model model) {
