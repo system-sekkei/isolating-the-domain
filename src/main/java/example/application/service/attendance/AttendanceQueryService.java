11c4312ffe36367e4fad4ea46b34411bfc145ca0
irof <irof@hogedriven.net>
2018-12-21 16:38:31 +0900

    未使用削除

--- src/main/java/example/application/service/attendance/AttendanceQueryService.java
+++ src/main/java/example/application/service/attendance/AttendanceQueryService.java
@@ -1,11 +1,9 @@
 package example.application.service.attendance;
 
 import example.application.repository.AttendanceRepository;
-import example.domain.model.attendance.Attendances;
 import example.domain.model.attendance.MonthlyAttendances;
 import example.domain.model.attendance.WorkMonth;
 import example.domain.model.worker.WorkerNumber;
-import example.domain.type.date.Date;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,14 +20,6 @@ public class AttendanceQueryService {
     public MonthlyAttendances findMonthlyAttendances(WorkerNumber workerNumber, WorkMonth month) {
         return attendanceRepository.findMonthly(workerNumber, month);
     }
-
-    /**
-     * 勤務時間取得
-     */
-    public Attendances getAttendances(WorkerNumber workerNumber, Date startDate, Date endDate) {
-        return attendanceRepository.getAttendances(workerNumber, startDate, endDate);
-    }
-
 
     AttendanceQueryService(AttendanceRepository attendanceRepository) {
         this.attendanceRepository = attendanceRepository;
