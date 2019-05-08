9c971bc0c00fa28738d19de6acbe9e975d716a6a
k_taniguchi <taniguchi.kiminobu@actier.co.jp>
2019-05-08 18:46:43 +0900

    formatterを導入

--- mobile-client/MEMO.md
+++ mobile-client/MEMO.md
@@ -1,6 +1,7 @@
 # mobile SPA client (by Elm)
 
 ## 情報源
+
 * official: https://elm-lang.org/
     * official導入ガイド: https://elm-lang.org/docs
     * chat(slack): http://elmlang.herokuapp.com/
@@ -20,6 +21,7 @@
         * [miyamo_madoka - Qiita](https://qiita.com/miyamo_madoka)
 
 ## CSSの取り扱い
+
 * 選択肢
     1.  CSS in Elm (独自実装)
         * [CSS in JS(Elm)したら想像以上に良かった - ジンジャー研究室](http://jinjor-labo.hatenablog.com/entry/2016/05/30/165816)
@@ -40,13 +42,29 @@
     * 2, 3, 4に関しては組織やコミュニティが提供しているものではなさそうなので、導入には勇気がいる。特に3, 4はマークアップでレイアウトを組むので、html = 論理的な構造の定義、css = レイアウトの定義という役割の分離がなくなってしまい、最終的に出力されるhtmlが装飾で汚れるので個人的に好みではない。2については使ってみないとよくわからない感じがする
 
 ## 開発環境
+
 * ツール
     * plugin対応IDE: intellij, vscode
 * エコシステム
     * パッケージリポジトリ: https://package.elm-lang.org/
-* wip…
+    * `elm` コマンドはnpmでインストールできるので、[node-gradle/gradle-node-plugin](https://github.com/node-gradle/gradle-node-plugin) を使って容易にgradleのタスクに乗せることができる。開発環境/CI環境にはjavaさえ入っていれば良いという状況は作れる
 
 ## モジュールの考え方
+
+* 基本はページ単位でモジュールを作るのがよさそう
 * 参考実装
     *  [GitHub - elm/package.elm-lang.org: website for browsing packages and exploring documentation](https://github.com/elm/package.elm-lang.org)
     * [GitHub - rtfeldman/elm-spa-example: A Single Page Application written in Elm](https://github.com/rtfeldman/elm-spa-example)
+
+## MISC
+
+### PWA対応
+
+* ServiceWorkerを使わない場合は、エントリポイントになるhtmlでmanifestファイルを読み込むだけなので、特に問題はない
+* ServiceWorkerの登録はJSで。中の処理はelmのworkerかける？
+    * refs. https://discourse.elm-lang.org/t/psa-elm-http-works-with-serviceworker/2562
+    * refs. https://notes.eellson.com/2018/02/26/offline-post-requests-with-elm-and-service-worker/
+
+### localstrage
+
+* refs. [複数のElmアプリで小さなデータを共有する - Local storage - Qiita](https://qiita.com/sand/items/3767d263f98b3dad264e)
--- mobile-client/src/Main.elm
+++ mobile-client/src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view, viewLink)
 
 import Browser
 import Browser.Navigation as Nav
@@ -6,28 +6,31 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Url
 
+
 main =
     Browser.application
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    , onUrlChange = UrlChanged
-    , onUrlRequest = LinkClicked
-    }
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
 
 -- MODEL
 
 
 type alias Model =
-  { key : Nav.Key
-  , url : Url.Url
-  }
+    { key : Nav.Key
+    , url : Url.Url
+    }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  ( Model key url, Cmd.none )
+    ( Model key url, Cmd.none )
 
 
 
@@ -35,25 +38,25 @@ init flags url key =
 
 
 type Msg
-  = LinkClicked Browser.UrlRequest
-  | UrlChanged Url.Url
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    LinkClicked urlRequest ->
-      case urlRequest of
-        Browser.Internal url ->
-          ( model, Nav.pushUrl model.key (Url.toString url) )
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
 
-        Browser.External href ->
-          ( model, Nav.load href )
+                Browser.External href ->
+                    ( model, Nav.load href )
 
-    UrlChanged url ->
-      ( { model | url = url }
-      , Cmd.none
-      )
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
 
 
 
@@ -62,7 +65,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  Sub.none
+    Sub.none
 
 
 
@@ -71,21 +74,21 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view model =
-  { title = "URL Interceptor"
-  , body =
-      [ text "The current URL is: "
-      , b [] [ text (Url.toString model.url) ]
-      , ul []
-          [ viewLink "/home"
-          , viewLink "/profile"
-          , viewLink "/reviews/the-century-of-the-self"
-          , viewLink "/reviews/public-opinion"
-          , viewLink "/reviews/shah-of-shahs"
-          ]
-      ]
-  }
+    { title = "URL Interceptor"
+    , body =
+        [ text "The current URL is: "
+        , b [] [ text (Url.toString model.url) ]
+        , ul []
+            [ viewLink "/home"
+            , viewLink "/profile"
+            , viewLink "/reviews/the-century-of-the-self"
+            , viewLink "/reviews/public-opinion"
+            , viewLink "/reviews/shah-of-shahs"
+            ]
+        ]
+    }
 
 
 viewLink : String -> Html msg
 viewLink path =
-  li [] [ a [ href path ] [ text path ] ]
+    li [] [ a [ href path ] [ text path ] ]
