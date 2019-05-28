911599b369c7ce18d9a04d96cec6b6616ad74e2c
k_taniguchi <taniguchi.kiminobu@actier.co.jp>
2019-05-28 14:44:11 +0900

    ダッシュボードにリンク追加

--- mobile-client/src/Pages/Dashboard.elm
+++ mobile-client/src/Pages/Dashboard.elm
@@ -2,7 +2,9 @@ module Pages.Dashboard exposing (Model, init, view)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Types.Employee.EmployeeNumber exposing (EmployeeNumber(..))
 import Types.Time.ClientTime exposing (ClientTime)
+import Types.Time.WorkDate as WorkDate
 import Types.Time.YearMonth as YearMonth
 import URLs
 
@@ -35,7 +37,8 @@ view model =
         , case model.state of
             Loaded clientTime ->
                 ul []
-                    [ routerLink (YearMonth.from clientTime |> URLs.payrollPageURL) "給与の一覧"
+                    [ routerLink (URLs.payrollPageURL (YearMonth.from clientTime)) "給与の一覧"
+                    , routerLink (URLs.timerecordPageURL (EmployeeNumber 1) (WorkDate.from clientTime)) "勤務時間の入力"
                     , routerLink "/xxxxx" "Not found var1"
                     , routerLink "/32-4098-12/;;asdkl;" "Not found var2"
                     ]
