77892b9842a3539e883ddb6a05d897cf2c6338d2
irof <irof.ocean@gmail.com>
2019-10-23 00:45:37 +0900

    javadoc

--- webapp/src/main/java/example/domain/model/attendance/package-info.java
+++ webapp/src/main/java/example/domain/model/attendance/package-info.java
@@ -1,4 +1,6 @@
 /**
- * 勤怠
+ * 給与単位の勤怠
+ *
+ * 1ヶ月の勤務記録を束ね、出勤欠勤を扱う。
  */
 package example.domain.model.attendance;
---
+++ webapp/src/main/java/example/domain/model/timerecord/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 勤務単位の記録
+ *
+ * 出勤から退勤までの時間を扱う。
+ */
+package example.domain.model.timerecord;
