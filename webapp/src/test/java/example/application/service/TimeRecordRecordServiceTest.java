0aa9542fcac86f417315c11a314b0ecfcda9ed50
irof <irof.ocean@gmail.com>
2019-11-13 17:18:34 +0900

    落ちるテストを追加

--- webapp/src/test/java/example/application/service/TimeRecordRecordServiceTest.java
+++ webapp/src/test/java/example/application/service/TimeRecordRecordServiceTest.java
@@ -5,7 +5,6 @@ import example.application.service.employee.EmployeeQueryService;
 import example.application.service.timerecord.TimeRecordRecordService;
 import example.domain.model.attendance.Attendance;
 import example.domain.model.attendance.WorkMonth;
-import example.domain.model.employee.ContractingEmployees;
 import example.domain.model.employee.Employee;
 import example.domain.model.employee.EmployeeNumber;
 import example.domain.model.timerecord.evaluation.ActualWorkDateTime;
@@ -15,13 +14,15 @@ import example.presentation.controller.timerecord.AttendanceForm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * 勤怠情報のテスト
  */
 @SpringBootTest
+@Transactional
 class TimeRecordRecordServiceTest {
 
     @Autowired
@@ -33,20 +34,35 @@ class TimeRecordRecordServiceTest {
 
     @Test
     void 登録した勤怠情報を取得できる() {
-        ContractingEmployees employees = employeeQueryService.contractingEmployees();
-        Employee employee = employees.list().get(0);
-        EmployeeNumber employeeNumber = employee.employeeNumber();
+        EmployeeNumber employeeNumber = new EmployeeNumber(1);
+        Employee employee = employeeQueryService.choose(employeeNumber);
 
-        ActualWorkDateTime actualWorkDateTime = AttendanceForm.toActualWorkDateTime("2017-10-20", "9:00", "24:00", "60", "0");
+        TimeRecord registerTimeRecord = new TimeRecord(employeeNumber, AttendanceForm.toActualWorkDateTime("2000-10-20", "9:00", "25:00", "60", "30"));
+        timeRecordRecordService.registerTimeRecord(registerTimeRecord);
 
-        TimeRecord expectTimeRecord = new TimeRecord(employeeNumber, actualWorkDateTime);
-        timeRecordRecordService.registerTimeRecord(expectTimeRecord);
+        Attendance attendance = attendanceQueryService.findAttendance(employee, new WorkMonth("2000-10"));
+        TimeRecord timeRecord = attendance.at(new WorkDate("2000-10-20"));
+        ActualWorkDateTime actualWorkDateTime = timeRecord.actualWorkDateTime();
 
-        Attendance attendance = attendanceQueryService.findAttendance(employee, new WorkMonth("2017-10"));
-        assertAll(
-                () -> assertEquals(attendance.month().toStringWithUnit(), "2017年10月"),
-                () -> assertEquals(attendance.listWorkDates().size(), 31),
-                () -> assertTrue(attendance.statusOf(new WorkDate("2017-10-20")).isWork())
-        );
+        assertEquals("14時間30分", actualWorkDateTime.workTime().toString());
+    }
+
+    @Test
+    void 同じ日の勤怠情報を登録すると更新される() {
+        登録した勤怠情報を取得できる();
+
+        EmployeeNumber employeeNumber = new EmployeeNumber(1);
+        Employee employee = employeeQueryService.choose(employeeNumber);
+
+        timeRecordRecordService.registerTimeRecord(
+                new TimeRecord(employeeNumber, AttendanceForm.toActualWorkDateTime("2000-10-20", "8:30", "25:00", "60", "30")));
+        timeRecordRecordService.registerTimeRecord(
+                new TimeRecord(employeeNumber, AttendanceForm.toActualWorkDateTime("2000-10-20", "10:30", "25:00", "60", "30")));
+
+        Attendance attendance = attendanceQueryService.findAttendance(employee, new WorkMonth("2000-10"));
+        TimeRecord timeRecord = attendance.at(new WorkDate("2000-10-20"));
+        ActualWorkDateTime actualWorkDateTime = timeRecord.actualWorkDateTime();
+
+        assertEquals("13時間00分", actualWorkDateTime.workTime().toString());
     }
 }
