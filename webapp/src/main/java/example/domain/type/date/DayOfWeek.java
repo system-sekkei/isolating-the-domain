6ef8908944c562cf6e47b55dc48fe388dc44d5b9
Abe Asami <sammy@nocono.net>
2019-12-18 20:50:27 +0900

    週の始まりは日曜日

--- webapp/src/main/java/example/domain/type/date/DayOfWeek.java
+++ webapp/src/main/java/example/domain/type/date/DayOfWeek.java
@@ -4,13 +4,13 @@ package example.domain.type.date;
  * 曜日
  */
 public enum DayOfWeek {
+    日(java.time.DayOfWeek.SUNDAY),
     月(java.time.DayOfWeek.MONDAY),
     火(java.time.DayOfWeek.TUESDAY),
     水(java.time.DayOfWeek.WEDNESDAY),
     木(java.time.DayOfWeek.THURSDAY),
     金(java.time.DayOfWeek.FRIDAY),
-    土(java.time.DayOfWeek.SATURDAY),
-    日(java.time.DayOfWeek.SUNDAY);
+    土(java.time.DayOfWeek.SATURDAY);
 
     private final java.time.DayOfWeek dayOfWeek;
 
