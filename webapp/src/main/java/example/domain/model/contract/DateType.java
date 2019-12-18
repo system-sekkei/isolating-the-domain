e2ac24935ba9bb1839b116f32ac88170c8c1481a
Abe Asami <sammy@nocono.net>
2019-12-19 01:54:12 +0900

    勤務日付に労働日か休日かのEnumフィールドを追加

---
+++ webapp/src/main/java/example/domain/model/contract/DateType.java
@@ -0,0 +1,9 @@
+package example.domain.model.contract;
+
+/**
+ * 労働日／休日
+ */
+public enum DateType {
+    労働日,
+    休日;
+}
--- webapp/src/main/java/example/domain/model/timerecord/evaluation/WorkDate.java
+++ webapp/src/main/java/example/domain/model/timerecord/evaluation/WorkDate.java
@@ -1,5 +1,6 @@
 package example.domain.model.timerecord.evaluation;
 
+import example.domain.model.contract.DateType;
 import example.domain.validation.Required;
 import example.domain.type.date.Date;
 import example.domain.type.date.DayOfWeek;
@@ -16,12 +17,15 @@ public class WorkDate {
     @NotNull(message = "勤務日を入力してください", groups = Required.class)
     Date value;
 
+    DateType dateType;
+
     @Deprecated
     public WorkDate() {
     }
 
     public WorkDate(Date date) {
         value = date;
+        this.dateType = DateType.労働日; // FIXME: とりあえずフィールドを作ってみただけなので、仮値を設定している
     }
 
     public static WorkDate from(String value) {
