871d768c4df4c1b379486630c8257ca685bf7024
irof <irof.ocean@gmail.com>
2020-03-27 11:53:39 +0900

    remove mobile-client

--- mobile-client/src/app.js
+++
@@ -1,8 +0,0 @@
-require('./assets/css/bluma-customize.scss');
-require('@mdi/font/css/materialdesignicons.css');
-
-const {Elm} = require('./Main.elm');
-
-Elm.Main.init({
-    node: document.getElementById('elm')
-});
