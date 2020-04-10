b4c0fa9e63f36e8fbd47eea122a2ca37935a40ef
Abe Asami <sammy@nocono.net>
2020-04-10 20:40:29 +0900

    契約適用期間を作成

---
+++ webapp/src/main/java/example/domain/model/contract/ContractPeriod.java
@@ -0,0 +1,9 @@
+package example.domain.model.contract;
+
+/**
+ * 契約適用期間
+ */
+public class ContractPeriod {
+    ContractEffectiveDate contractEffectiveDate;
+    ContractTerminateDate contractTerminateDate;
+}
---
+++ webapp/src/main/java/example/domain/model/contract/ContractTerminateDate.java
@@ -0,0 +1,10 @@
+package example.domain.model.contract;
+
+import example.domain.type.date.Date;
+
+/**
+ * 契約終了日
+ */
+public class ContractTerminateDate {
+    Date value;
+}
