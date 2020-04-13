c98942c672736a380316d5135a917dd5073f3129
Abe Asami <sammy@nocono.net>
2020-04-13 20:20:29 +0900

    Revert "契約適用期間を作成"
    This reverts commit b4c0fa9e63f36e8fbd47eea122a2ca37935a40ef.
    
    契約終了はこの給与システムでは不要であるため。

--- webapp/src/main/java/example/domain/model/contract/ContractPeriod.java
+++
@@ -1,9 +0,0 @@
-package example.domain.model.contract;
-
-/**
- * 契約適用期間
- */
-public class ContractPeriod {
-    ContractEffectiveDate contractEffectiveDate;
-    ContractTerminateDate contractTerminateDate;
-}
